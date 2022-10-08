67a9c79cb16d0db2b9e00b96aaf7ed1e10ecf263
Simon Ho <simonho.ubc@gmail.com>
2022-10-07 23:10:33 -0700

    Add toast notification

---
+++ Helpers/DialogHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace SharpCore.Helpers
+{
+    public class DialogHelper
+    {
+        /// <summary>
+        /// Show a Windows toast notification
+        /// </summary>
+        /// <param name="header">Text for the header</param>
+        /// <param name="body">Text for the main body</param>
+        /// <param name="image">URI for an image to be displayed</param>
+        /// <param name="expiration">Time, in seconds, after which the notification expires</param>
+        public static void ShowToast(string header, string body, Uri? image = null, int expiration = 60)
+        {
+            ToastContentBuilder toast = new ToastContentBuilder()
+                .AddText(header)
+                .AddText(body);
+
+            if (image != null)
+            {
+                toast.AddInlineImage(image);
+            }
+
+            toast.Show(toast =>
+            {
+                toast.ExpirationTime = DateTime.Now.AddSeconds(expiration);
+            });
+        }
+    }
+
+}
