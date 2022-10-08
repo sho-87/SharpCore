9fc5036ad21d8f86e78db9a5f7d9c5088e81a080
Simon Ho <simonho.ubc@gmail.com>
2022-10-08 01:49:20 -0700

    Move overlay method to extension

---
+++ Extensions/ImageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SharpCore.Helpers;
+
+public static class ImageExtensions
+{
+    /// <summary>
+    /// Overlay one image ontop of another
+    /// </summary>
+    /// <param name="baseImage">Bottom image</param>
+    /// <param name="overlayImage">Top image</param>
+    /// <param name="overlayScale">Scale of the top image</param>
+    /// <param name="darken">Whether the resulting image should be darkened</param>
+    /// <returns>Base image with the overlay image on top</returns>
+    public static Image AddOverlayImage(this Image baseImage, Image overlayImage, double overlayScale = 1.0, bool darken = false)
+    {
+        overlayImage = new Bitmap(overlayImage, (int)(overlayImage.Width * overlayScale), (int)(overlayImage.Height * overlayScale));
+
+        var result = new Bitmap(baseImage.Width, baseImage.Height, PixelFormat.Format32bppPArgb);
+
+        using var g = Graphics.FromImage(result);
+        g.DrawImage(baseImage, 0, 0);
+        g.DrawImage(overlayImage, baseImage.Width / 2 - overlayImage.Width / 2, baseImage.Height / 2 - overlayImage.Height / 2);
+
+        if (darken)
+        {
+            using Brush darkBrush = new SolidBrush(Color.FromArgb(128, Color.Black));
+            g.FillRectangle(darkBrush, new Rectangle(0, 0, baseImage.Width, baseImage.Height));
+        }
+
+        return (Image)result;
+    }
+}
--- Helpers/ImageHelper.cs
+++ Helpers/ImageHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace SharpCore.Helpers;
 
@@ -31,32 +30,5 @@ public class ImageHelper
 
         decimal rnd = Math.Min(maxWidth / (decimal)src.Width, maxHeight / (decimal)src.Height);
         return new Size((int)Math.Round(src.Width * rnd), (int)Math.Round(src.Height * rnd));
-    }
-
-    /// <summary>
-    /// Overlay one image ontop of another
-    /// </summary>
-    /// <param name="baseImage">Bottom image</param>
-    /// <param name="overlayImage">Top image</param>
-    /// <param name="overlayScale">Scale of the top image</param>
-    /// <param name="darken">Whether the resulting image should be darkened</param>
-    /// <returns>Base image with the overlay image on top</returns>
-    public static Bitmap OverlayImages(Image baseImage, Image overlayImage, double overlayScale = 1.0, bool darken = false)
-    {
-        overlayImage = new Bitmap(overlayImage, (int)(overlayImage.Width * overlayScale), (int)(overlayImage.Height * overlayScale));
-
-        var result = new Bitmap(baseImage.Width, baseImage.Height, PixelFormat.Format32bppPArgb);
-
-        using var g = Graphics.FromImage(result);
-        g.DrawImage(baseImage, 0, 0);
-        g.DrawImage(overlayImage, baseImage.Width / 2 - overlayImage.Width / 2, baseImage.Height / 2 - overlayImage.Height / 2);
-
-        if (darken)
-        {
-            using Brush darkBrush = new SolidBrush(Color.FromArgb(128, Color.Black));
-            g.FillRectangle(darkBrush, new Rectangle(0, 0, baseImage.Width, baseImage.Height));
-        }
-
-        return result;
     }
 }
