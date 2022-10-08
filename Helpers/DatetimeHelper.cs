ba04501f49c858981609fdf42ee2e1c6e315c0c7
Simon Ho <simonho.ubc@gmail.com>
2022-10-07 21:54:13 -0700

    Add datetime helper

---
+++ Helpers/DatetimeHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpCore.Helpers;
+
+public class DatetimeHelper
+{
+    /// <summary>
+    /// Get the current Unix time in milliseconds
+    /// </summary>
+    /// <returns>Unix time in milliseconds</returns>
+    public static long GetCurrentUnixTime()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        return now.ToUnixTimeMilliseconds();
+    }
+}
