37d5ce5b77a3d0261a6dc62f0110f895a99d3973
Simon Ho <simonho.ubc@gmail.com>
2022-10-14 10:02:39 -0700

    Add visibility converter

---
+++ Converters/BoolConverters.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace SharpCore.Converters;
+
+public class BoolToVisibility : IValueConverter
+{
+    /// <summary>
+    /// Returns a visibility state
+    /// </summary>
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if ((bool)value)
+        {
+            return Microsoft.UI.Xaml.Visibility.Visible;
+        } else
+        {
+            return Microsoft.UI.Xaml.Visibility.Collapsed;
+        }
+    }
+
+    /// <summary>
+    /// Returns a bool
+    /// </summary>
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
