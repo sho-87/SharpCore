4613cf61a892347cd01fe238d0dfbfc739b0d9ed
Simon Ho <simonho.ubc@gmail.com>
2022-10-21 17:27:56 -0700

    Add scrollviewer pan helper

---
+++ Helpers/ControlHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+
+namespace SharpCore;
+
+public class ControlHelper
+{
+    /// <summary>
+    /// Pan a scroll viewer control based on manipulation
+    /// </summary>
+    /// <param name="sv">Target scrollviewer</param>
+    /// <param name="e">Manipulation move event</param>
+    public static void PanScrollViewer(ScrollViewer sv, ManipulationDeltaRoutedEventArgs e)
+    {
+        double TranX = -1 * e.Delta.Translation.X;
+        double TranY = -1 * e.Delta.Translation.Y;
+
+        double OffsetX = TranX + sv.HorizontalOffset;
+        double OffsetY = TranY + sv.VerticalOffset;
+
+        sv.ChangeView(OffsetX, OffsetY, null, true);
+    }
+}
